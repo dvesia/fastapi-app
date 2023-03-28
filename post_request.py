d910f89c1449d461bc411d7b7e27d968a66a76e9
Domenico Vesia <dvesia@MacBook-Air-di-Domenico.local>
2023-03-28 22:16:00 +0200

    Add cd action

---
+++ post_request.py
@@ -0,0 +1,20 @@
+import requests
+
+api_url = "https://fastapi-app-dvesia.onrender.com"
+
+data = {
+    "age": 19,
+    "workclass": "Private",
+    "education": "HS-grad",
+    "maritalStatus": "Never-married",
+    "occupation": "Other-service",
+    "relationship": "Own-child",
+    "race": "Black",
+    "sex": "Male",
+    "hoursPerWeek": 40,
+    "nativeCountry": "United-States"
+}
+
+response = requests.post(api_url, json=data)
+print(response)
+print("Response content:", response.content)
